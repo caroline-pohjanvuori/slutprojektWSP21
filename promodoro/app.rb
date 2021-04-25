e1dd667684e074e45d447e27d92055e930028502
caroline-pohjanvuori <55193787+caroline-pohjanvuori@users.noreply.github.com>
2021-04-25 22:08:51 +0200

    eeeeeeee
    yes

--- promodoro/app.rb
+++ promodoro/app.rb
@@ -7,6 +7,7 @@ require 'bcrypt'
 enable :sessions
 
 def connect_to_db(path)
+  #funk för att effektivare connecta till db
   db = SQLite3::Database.new(path)
   db.results_as_hash = true
   return db
@@ -17,84 +18,103 @@ get('/') do
   slim(:"/index")
 end
 
-#post('/users/new')do
- # username= params[:username]
-  #password=params[:password]
-  #password_comfirm=params[:password_comfirm]
+get('/showregister') do
+  slim(:"/register")
+end
 
-  #if (password_comfirm == password)
+post('/users/new')do
+  #nya användare
+  username= params[:username]
+  password=params[:password]
+  password_comfirm=params[:password_comfirm]
+
+  if (password_comfirm == password)
     #lägg till användare
-    #password_digest= BCrypt::Password.create(password)
-    #db=SQLite3::Database.new('db/Todo2020.db') 
-    #db.execute("INSERT INTO users (username, pwdigest) VALUES (?,?)", username, password_digest)
-    #redirect('/') 
-  #else
-    ##error
-    #"Lösenordet matchade ej"
-  #end
-#end
+    password_digest= BCrypt::Password.create(password)
+    db = connect_to_db('db/promodoro.db') 
+    db.execute("INSERT INTO user (username, pwdigest) VALUES (?,?)", username, password_digest)
+    redirect('/') 
+  else
+    #error
+    "Lösenordet matchade ej"
+  end
+end
+get('/showlogin') do
+  #mellansteg till login
+  slim(:login)
+end
 
-get('/task/tasklist') do
+post('/login') do
+  #login för gamla användare
+  username= params[:username]
+  password=params[:password]
+  db = connect_to_db('db/promodoro.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM user WHERE username = ?", username).first
+  pwdigest= result["pwdigest"]
+  id= result["id"]
+
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id] = id
+    redirect('/')
+  else
+    #error
+    "lösenordet matchade ej"
+  end
+end
+
+get('/tasklist') do
+  #tar upp tasks
   db = connect_to_db('db/promodoro.db')
   id = session[:id].to_i
   result = db.execute("SELECT * FROM tasks WHERE userid = ?", id)
-  p "alla results fran #{result}"
-  slim(:"/tasklist", locals:{tasks:result})
+  slim(:"task/tasklist", locals:{tasks:result})
 end
 
-#get('/showlogin') do
- # slim(:login)
-#end
-#post('/login') do
- # username= params[:username]
-  #password=params[:password]
-  #db=SQLite3::Database.new('db/Todo2020.db') 
-  #db.results_as_hash = true
-  #result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-  #pwdigest= result["pwdigest"]
-  #id= result["id"]
-
-  #if BCrypt::Password.new(pwdigest) == password
-    #session[:id] = id
-    #redirect('/todos')
-  #else
-    ##error
-    #"lösenordet matchade ej"
-  #end
-#end
 
 post('/tasklist') do
+  #skapar nya tasks
   newTask = params[:task]
   description= params[:description]
+  taskcolor = params[:col].to_i
   user_id = session[:id]
-  db=SQLite3::Database.new('db/promodoro.db') 
+  db = connect_to_db('db/promodoro.db')
   db.execute("INSERT INTO tasks (taskname, description, userid) VALUES (?, ?, ?)", newTask, description, user_id)
-  redirect('/tasks') 
+  result = db.execute("SELECT id FROM tasks WHERE taskname=? AND userId=? ", newTask, user_id).first
+  id = result["id"].to_i
+  db.execute("INSERT INTO taskcolor (taskid, colorid) VALUES (?, ?)", id, taskcolor)
+  redirect('/tasklist') 
 end
 
 
-get('/taskList/:id/edit') do
+get('/tasklist/:id/edit') do
+  #första steg för att edita tasks. Det som händer när du trycker på knappen
   id = params[:id].to_i
-  db=SQLite3::Database.new('db/promodoro.db') 
+  db = connect_to_db('db/promodoro.db') 
   db.results_as_hash = true
   result = db.execute("SELECT * FROM tasks WHERE id = ?", id).first
-  slim(:"/tasks/edit", locals:{result:result})
+  slim(:"/task/edit", locals:{update:result})
 end
 
 post('/taskList/:id/update') do
-  content= params[:updatedTask]
-  content= params[:updateddesc]
+  #andra steget för att edita tasks. Det som händer när du har skrivit in uppdateringarna
+  taskname= params[:updatedtask]
+  description = params[:updateddesc]
+  color = params[:col].to_i
   id=params[:id].to_i
-  db=SQLite3::Database.new('db/promodoro.db') 
-  db.execute("UPDATE tasks SET taskcontent = ? WHERE id = ?", content, id)
-  redirect('/tasks')
+  db = connect_to_db('db/promodoro.db') 
+  db.execute("UPDATE tasks SET taskname = ?, description = ?  WHERE id = ?", taskname, description, id)
+  db.execute("UPDATE taskcolor SET colorid = ? WHERE taskid = ?", color, id)
+  redirect('/tasklist')
 end
 
 
-post('/taskList/:id/delete') do
+post('/tasklist/:id/delete') do
+  #radera tasks
   id= params[:id].to_i
-  db=SQLite3::Database.new('db/promodoro.db') 
+  db = connect_to_db('db/promodoro.db') 
   db.execute("DELETE FROM tasks WHERE id = ?", id)
-  redirect('/tasks')
+  db.execute("DELETE FROM taskcolor WHERE taskid = ?", id)
+  redirect('/tasklist')
 end
 
